1f4defe2a627be2794e3a41fc50499be0c50169e
Brian Holt <bholt@dwolla.com>
2025-12-02 15:57:29 -0600

    namespace Dwolla-specific keys

--- dwolla-xray-annotations/src/main/scala/com/dwolla/tracing/XrayAnnotations.scala
+++ dwolla-xray-annotations/src/main/scala/com/dwolla/tracing/XrayAnnotations.scala
@@ -5,16 +5,18 @@ sealed trait XRayAnnotationKey
 sealed trait ClientAccountId extends XRayAnnotationKey
 sealed trait EndUserAccountId extends XRayAnnotationKey
 sealed trait TransactionId extends XRayAnnotationKey
+sealed trait EventBusMessageResult extends XRayAnnotationKey
 
 object XRayAnnotationKey {
   val clientAccountId: String with ClientAccountId = key("client.account.id")
   val endUserAccountId: String with EndUserAccountId = key("enduser.account.id")
   val transactionId: String with TransactionId = key("transaction.id")
+  val eventBusMessageResult: String with EventBusMessageResult = key("event-bus.message-handling-result")
 
   private lazy val registry = Set.newBuilder[String with XRayAnnotationKey]
 
   private def key[T <: XRayAnnotationKey](s: String): String with T = {
-    val t = s.asInstanceOf[String with T]
+    val t = s"com.dwolla.$s".asInstanceOf[String with T]
     registry += t
     t
   }
